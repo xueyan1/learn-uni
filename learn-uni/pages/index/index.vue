697ceca639caf43af7a11defb7b2f521c7e3e75f
xueyan <348352046@qq.com>
2019-03-20 16:29:08 +0800

    update

---
+++ learn-uni/pages/index/index.vue
@@ -0,0 +1,40 @@
+<template>
+	<view class="content">
+        <image class="logo" src="http://www.pptbz.com/pptpic/UploadFiles_6909/201203/2012031220134655.jpg"></image>
+		<view>
+            <text class="title">{{title}}</text>
+        </view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: '首页'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		text-align: center;
+		height: 400upx;
+	}
+    .logo{
+        height: 200upx;
+        width: 200upx;
+        margin-top: 200upx;
+    }
+	.title {
+		font-size: 36upx;
+		color: #8f8f94;
+	}
+</style>
